b3b5cd6b68c0f302e668b4defb985079ca4f9716
Loup Aubour <laubour@arneogroup.com>
2022-01-07 17:48:57 +0100

    form's input's ids implemented / #2

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,7 +11,10 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalBody = document.querySelector(".modal-body");
+const form = document.querySelector("form");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -21,8 +24,32 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-// close modal form
-const close = document.querySelector('.close');
-close.addEventListener('click', (e)=> {
+// #1st Issue : close modal form
+closeBtn.addEventListener('click', (e)=> {
   modalbg.style.display = "none";
 })
+
+// #2nd Issue : implement form inputs
+
+// retrieving form's input's IDs
+
+const allInputs = form.querySelectorAll("input");
+
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const email = document.getElementById("email");
+const birthdate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+const chosenCity = document.getElementById("chooseYourCity");
+const termsOfUseCheckbox = document.getElementById("checkbox1");
+const newsletterCheckbox = document.getElementById("checkbox2");
+
+const submitBtn = document.querySelector('.btn-submit');
+
+// validation au moment du clique sur "SUBMIT"
+form.addEventListener('submit', validate);
+
+// form validation function
+function validate(e) {
+  e.preventDefault();
+}
