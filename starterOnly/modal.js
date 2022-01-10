ebdecb5898df590343f11a3e5b39060dadb536f6
Loup Aubour <laubour@arneogroup.com>
2022-01-10 09:31:08 +0100

    Add thank you modal when form validation ok / #4

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -17,8 +17,21 @@ const form = document.querySelector("form");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelector(".close");
 
+// Form Inputs / retrieving form's input's IDs
+const allInputs = form.querySelectorAll("input");
 
-// modal behaviour
+const firstInput = document.getElementById("first");
+const lastInput = document.getElementById("last");
+const emailInput = document.getElementById("email");
+const birthdateInput = document.getElementById("birthdate");
+const quantityInput = document.getElementById("quantity");
+const chosenCity = document.getElementById("chosenCity");
+const termsOfUseCheckbox = document.getElementById("checkbox1");
+const newsletterCheckbox = document.getElementById("checkbox2");
+
+const submitBtn = document.querySelector('.btn-submit');
+
+///// Modal Behaviour /////
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -46,25 +59,10 @@ closeBtn.addEventListener('click', (e)=> {
 //                                     //
 /////////////////////////////////////////
 
-// retrieving form's input's IDs
-const allInputs = form.querySelectorAll("input");
-
-const firstInput = document.getElementById("first");
-const lastInput = document.getElementById("last");
-const emailInput = document.getElementById("email");
-const birthdateInput = document.getElementById("birthdate");
-const quantityInput = document.getElementById("quantity");
-const chosenCity = document.getElementById("chooseYourCity");
-const termsOfUseCheckbox = document.getElementById("checkbox1");
-const newsletterCheckbox = document.getElementById("checkbox2");
-
-const submitBtn = document.querySelector('.btn-submit');
-
-// form is submitted when we click on "submitBtn"
-form.addEventListener('submit', validate);
-
-// form validation function
-function validate() {
+// form validation check function
+function formValidationCheck() {
+  validationRequired = true;
+  deleteErrorMessages();
 
   // retrieve form's input's values
   const form = document.querySelector("form");
@@ -80,12 +78,12 @@ function validate() {
   const location = formData.get("location");
 
   // form validation's conditions
-  if (first.length <= 2) {
+  if (first.length < 2) {
     displayErrorMessage(firstInput, "Veuillez entrer 2 caractères ou plus.");
     return false;
   }
 
-  if (last.length <= 2) {
+  if (last.length < 2) {
     displayErrorMessage(lastInput, "Veuillez entrer 2 caractères ou plus.");
     return false;
   }
@@ -112,7 +110,7 @@ function validate() {
   }
 
   if (!termsOfUseCheckbox.checked) {
-    displayErrorMessage(termsOfUseCheckbox, "Veuillez cocher la case indiquant que vous acceptez les conditions d'utilisation.");
+    displayErrorMessage(newsletterCheckbox, "Veuillez cocher la case des conditions d'utilisation.");
     return false;
   }
 
@@ -129,7 +127,7 @@ function validate() {
 // error message creation
 function createErrorMessage() {
   const errorMessage = document.createElement("span");
-  errorMessage.classList.add("errorMessage");
+  errorMessage.classList.add("error-message");
   errorMessage.style.cssText = "color: #D8000C; font-size: 12px; line-height: 12px";
   return errorMessage;
 }
@@ -144,8 +142,8 @@ function displayErrorMessage(input, errorText) {
 
 // error message deletion
 function deleteErrorMessages() {
-  let errorMessages = document.querySelectorAll(".errorMessage");
-  // suppressing red border with a loop
+  let errorMessages = document.querySelectorAll(".error-message");
+  // suppressing red border and outline with a loop
   chosenCity.style.outline = "none";
   for (let i = 0; i < allInputs.length; i++) {
     allInputs[i].style.outline = "none";
@@ -164,14 +162,78 @@ for (let i = 0; i < allInputs.length; i++) {
   const input = allInputs[i];
   input.addEventListener("input", () => {
     if (validationRequired === false) return;
-    validate(false);
+    formValidationCheck();
   });
 }
 
-// if the form is not valid we prevent default form behaviour
+
+//////////////////////////////////////////
+//                                      //
+// 4th Issue : display modal TY message //
+//                                      //
+//////////////////////////////////////////
+
+// modal thank you message creation
+function createThankYouModal() {
+  // creating a new modal from the original one
+  const newModal = modalbg.cloneNode(true);
+  const modalBody = newModal.querySelector(".modal-body");
+  const closeIcon = newModal.querySelector(".close");
+  const form = modalBody.querySelector("form");
+
+  // removing the form from the modal
+  form.remove();
+
+  // new modal CSS
+  newModal.style.display = "block";
+  modalBody.style.cssText = "display: flex; flex-direction: column;";
+
+  // thank you paragraph creation
+  const thankYouText = document.createElement("p");
+  thankYouText.innerHTML = "Merci pour votre participation à notre tournoi !";
+  thankYouText.style.cssText = "text-align: center; padding: 36px 8px";
+
+  // thank you button creation
+  const thankYouButton = document.createElement("button");
+  thankYouButton.classList.add("btn-submit");
+  thankYouButton.classList.add("final-submit");
+  thankYouButton.style.cssText = "margin: 4px 12px; text-align: center;";
+  thankYouButton.innerHTML = "Revenir sur la page d'accueil";
+
+  // adding modal's content and the new modal in the DOM
+  modalBody.appendChild(thankYouText);
+  modalBody.appendChild(thankYouButton);
+  modalbg.after(newModal);
+
+  // closing the modal by clicking on the button or the close icon
+  thankYouButton.addEventListener("click", () => newModal.remove());
+  closeIcon.addEventListener("click", () => newModal.remove());
+}
+
+
+// thank you message display management
+const displayThankYouMessage = "displayThankYouMessage";
+
+// getting thank you message from the sessionStorage
+const getThankYouModal = sessionStorage.getItem(
+  displayThankYouMessage
+);
+
+if (getThankYouModal === "true") {
+  // prevent the modal display when the page reloads
+  sessionStorage.setItem(displayThankYouMessage, "false");
+  createThankYouModal();
+}
+
+
+// form validation management
 form.onsubmit = (event) => {
-  const valid = validate();
-  if (valid === false) {
+  const validationCheck = formValidationCheck();
+  if (validationCheck === false) {
+    // prevent form usual behaviour (page regeneration and data deletion)
     event.preventDefault();
+  } else {
+    // store the thank you message into sessionStorage
+    sessionStorage.setItem(displayThankYouMessage, "true");
   }
 };
