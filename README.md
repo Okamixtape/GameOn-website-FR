f9d2de605a5c6962f0224e285bd3c8a8989b71ee
Loup Aubour <loup.aubour@gmail.com>
2024-02-02 15:02:20 +0100

    README.md updated and JS code refactored

--- README.md
+++ README.md
@@ -1,5 +1,34 @@
-# Projet GameOn
-1. Forkez ce repo ;
-2. Il est conseillé d'utiliser VisualStudio Code et vous pouvez utiliser Docker, mais ce n'est pas obligatoire ;
-3. Il n'y a aucune dépendance ;
-4. Vous ne devez utiliser que du CSS personnalisé et du JavaScript pur, sans jQuery, Bootstrap ou autre librairie.
+# JS Landing Page / GameOn
+
+This project involves adding the missing JavaScript code to make a form fully functional.
+
+## Description
+
+The task is to complete the JavaScript code for the form. The remaining tasks are described in the issues on the GitHub repo. Here is the [original repository](https://github.com/OpenClassrooms-Student-Center/GameOn-website-FR).
+
+## Tasks
+
+Here are the tasks to be completed:
+
+1. Manual Testing
+2. Add confirmation when successful submission
+3. Add validation or error messages
+4. Implement form entries
+5. TODO: close the modal
+
+## Issues
+
+The original issues can be found [here](https://github.com/OpenClassrooms-Student-Center/GameOn-website-FR/issues)
+
+## How to Contribute
+
+When you submit your code, make sure to:
+
+- Work on a forked GitHub repo
+- Use separate files for HTML, CSS, and JavaScript
+- Always comment your code (describe each function and class, as well as parts of the code that require more details)
+- Manually test the features, form entries, and responsive display
+
+## Contact
+
+If you have any questions or issues, feel free to open an issue on GitHub.
--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,13 +1,10 @@
-// function for a responsive nav
+// Responsive nav function
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+  const x = document.getElementById("myTopnav");
+  x.className === "topnav"
+    ? (x.className += " responsive")
+    : (x.className = "topnav");
 }
-
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
@@ -17,222 +14,148 @@ const form = document.querySelector("form");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelector(".close");
 
-// Form Inputs / retrieving form's input's IDs
-const allInputs = form.querySelectorAll("input");
+// Form Inputs
+const allInputs = Array.from(form.querySelectorAll("input"));
 
-const firstInput = document.getElementById("first");
-const lastInput = document.getElementById("last");
-const emailInput = document.getElementById("email");
-const birthdateInput = document.getElementById("birthdate");
-const quantityInput = document.getElementById("quantity");
-const chosenCity = document.getElementById("chosenCity");
-const termsOfUseCheckbox = document.getElementById("checkbox1");
-const newsletterCheckbox = document.getElementById("checkbox2");
+const formElements = {
+  firstInput: document.getElementById("first"),
+  lastInput: document.getElementById("last"),
+  emailInput: document.getElementById("email"),
+  birthdateInput: document.getElementById("birthdate"),
+  quantityInput: document.getElementById("quantity"),
+  chosenCity: document.getElementById("chosenCity"),
+  termsOfUseCheckbox: document.getElementById("checkbox1"),
+  newsletterCheckbox: document.getElementById("checkbox2"),
+};
 
-const submitBtn = document.querySelector('.btn-submit');
+const submitBtn = document.querySelector(".btn-submit");
 
-///// Modal Behaviour /////
+// Modal Behaviour
+modalBtn.forEach((btn) => btn.addEventListener("click", () => (modalbg.style.display = "block")));
+closeBtn.addEventListener('click', () => (modalbg.style.display = "none"));
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
-
-/////////////////////////////////////
-//                                 //
-//  1st Issue : close modal form   //
-//                                 //
-/////////////////////////////////////
-
-// close modal form
-closeBtn.addEventListener('click', (e)=> {
-  modalbg.style.display = "none";
-})
-
-
-/////////////////////////////////////////
-//                                     //
-//  2nd Issue : implement form inputs  //
-//                                     //
-/////////////////////////////////////////
-
-// form validation check function
+// Form validation check function
 function formValidationCheck() {
-  validationRequired = true;
   deleteErrorMessages();
 
-  // retrieve form's input's values
-  const form = document.querySelector("form");
-
-  // use of the FormData interface to construct sets of key/value pairs
   const formData = new FormData(form);
 
-  const first = formData.get("first");
-  const last = formData.get("last");
-  const email = formData.get("email");
-  const birthdate = formData.get("birthdate");
-  const quantity = formData.get("quantity");
-  const location = formData.get("location");
+  const { first, last, email, birthdate, quantity, location } = Object.fromEntries(formData);
 
-  // form validation's conditions
-  if (first.length < 2) {
-    displayErrorMessage(firstInput, "Veuillez entrer 2 caractères ou plus.");
-    return false;
-  }
+  const errorMessages = {
+    first: "Veuillez entrer 2 caractères ou plus.",
+    last: "Veuillez entrer 2 caractères ou plus.",
+    email: "Veuillez saisir une adresse courriel valide.",
+    birthdate: "Veuillez entrer votre date de naissance.",
+    quantity: "Veuillez entrer un nombre.",
+    location: "Veuillez choisir une ville.",
+    termsOfUseCheckbox: "Veuillez cocher la case des conditions d'utilisation.",
+  };
 
-  if (last.length < 2) {
-    displayErrorMessage(lastInput, "Veuillez entrer 2 caractères ou plus.");
-    return false;
-  }
+  for (const [fieldName, fieldValue] of Object.entries(formElements)) {
+    if (fieldName === 'location' && fieldValue.value === null) {
+      displayErrorMessage(fieldValue, errorMessages[fieldName]);
+      return false;
+    }
 
-  if (!email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)) {
-    displayErrorMessage(emailInput, "Veuillez saisir une adresse courriel valide.");
-    return false;
-  }
+    if (fieldName === 'birthdate' && isNaN(new Date(fieldValue).getTime())) {
+      displayErrorMessage(fieldValue, errorMessages[fieldName]);
+      return false;
+    }
 
-  const dateTime = new Date(birthdate).getTime();
-  if (isNaN(dateTime)) {
-    displayErrorMessage(birthdateInput, "Veuillez entrer votre date de naissance.");
-    return false;
-  }
+    if (fieldName === 'quantity' && (isNaN(Number(fieldValue)) || fieldValue === "")) {
+      displayErrorMessage(fieldValue, errorMessages[fieldName]);
+      return false;
+    }
 
-  if (isNaN(new Number(quantity)) || quantity === "") {
-    displayErrorMessage(quantityInput, "Veuillez entrer un nombre.");
-    return false;
-  }
+    if (fieldName === 'email' && !fieldValue.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)) {
+      displayErrorMessage(fieldValue, errorMessages[fieldName]);
+      return false;
+    }
 
-  if (location === null) {
-    displayErrorMessage(chosenCity, "Veuillez choisir une ville.");
-    return false;
-  }
-
-  if (!termsOfUseCheckbox.checked) {
-    displayErrorMessage(newsletterCheckbox, "Veuillez cocher la case des conditions d'utilisation.");
-    return false;
+    if ((fieldName === 'first' || fieldName === 'last') && fieldValue.length < 2) {
+      displayErrorMessage(fieldValue, errorMessages[fieldName]);
+      return false;
+    }
   }
 
   return true;
 }
 
-
-///////////////////////////////////////
-//                                   //
-// 3rd Issue : error message display //
-//                                   //
-///////////////////////////////////////
-
-// error message creation
+// Error message creation
 function createErrorMessage() {
   const errorMessage = document.createElement("span");
   errorMessage.classList.add("error-message");
   return errorMessage;
 }
 
-// error message display
+// Error message display
 function displayErrorMessage(input, errorText) {
-  errorMessage = createErrorMessage();
+  const errorMessage = createErrorMessage();
   errorMessage.innerHTML = errorText;
   input.after(errorMessage);
   input.style.outline = "2px solid #D8000C";
 }
 
-// error message deletion
+// Error message deletion
 function deleteErrorMessages() {
-  let errorMessages = document.querySelectorAll(".error-message");
-  // suppressing red border and outline with a loop
-  chosenCity.style.outline = "none";
-  for (let i = 0; i < allInputs.length; i++) {
-    allInputs[i].style.outline = "none";
-  }
-  // suppressing in the DOM error messages
-  for (let i = 0; i < errorMessages.length; i++) {
-    errorMessages[i].remove();
-  }
+  const errorMessages = document.querySelectorAll(".error-message");
+  formElements.chosenCity.style.outline = "none";
+  allInputs.forEach((input) => (input.style.outline = "none"));
+  errorMessages.forEach((errorMessage) => errorMessage.remove());
 }
 
-// stop the validation if there is an error
-let validationRequired = false;
-
-// checking every input without validating the form
-for (let i = 0; i < allInputs.length; i++) {
-  const input = allInputs[i];
+// Check input validation on input change
+allInputs.forEach((input) => {
   input.addEventListener("input", () => {
     if (validationRequired === false) return;
     formValidationCheck();
   });
-}
+});
 
-
-//////////////////////////////////////////
-//                                      //
-// 4th Issue : display modal TY message //
-//                                      //
-//////////////////////////////////////////
-
-// modal thank you message creation
+// Modal thank you message creation
 function createThankYouModal() {
-  // creating a new modal from the original one
   const newModal = modalbg.cloneNode(true);
   const modalBody = newModal.querySelector(".modal-body");
   const closeIcon = newModal.querySelector(".close");
   const form = modalBody.querySelector("form");
 
-  // removing the form from the modal
   form.remove();
-
-  // new modal CSS
+  
   newModal.style.display = "block";
   modalBody.style.cssText = "display: flex; flex-direction: column;";
 
-  // thank you paragraph creation
   const thankYouText = document.createElement("p");
   thankYouText.innerHTML = "Merci pour votre participation à notre tournoi !";
   thankYouText.style.cssText = "text-align: center; padding: 36px 8px";
 
-  // thank you button creation
   const thankYouButton = document.createElement("button");
   thankYouButton.classList.add("btn-submit");
   thankYouButton.classList.add("final-submit");
   thankYouButton.style.cssText = "margin: 4px 12px; text-align: center;";
   thankYouButton.innerHTML = "Revenir sur la page d'accueil";
 
-  // adding modal's content and the new modal in the DOM
   modalBody.appendChild(thankYouText);
   modalBody.appendChild(thankYouButton);
   modalbg.after(newModal);
 
-  // closing the modal by clicking on the button or the close icon
   thankYouButton.addEventListener("click", () => newModal.remove());
   closeIcon.addEventListener("click", () => newModal.remove());
 }
 
-
-// thank you message display management
 const displayThankYouMessage = "displayThankYouMessage";
-
-// getting thank you message from the sessionStorage
-const getThankYouModal = sessionStorage.getItem(
-  displayThankYouMessage
-);
+const getThankYouModal = sessionStorage.getItem(displayThankYouMessage);
 
 if (getThankYouModal === "true") {
-  // prevent the modal display when the page reloads
   sessionStorage.setItem(displayThankYouMessage, "false");
   createThankYouModal();
 }
 
-
-// form validation management
 form.onsubmit = (event) => {
   const validationCheck = formValidationCheck();
   if (validationCheck === false) {
-    // prevent form usual behaviour (page regeneration and data deletion)
     event.preventDefault();
   } else {
-    // store the thank you message into sessionStorage
     sessionStorage.setItem(displayThankYouMessage, "true");
   }
 };
